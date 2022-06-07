659b133c949df3eb9c3830897afd674159a472e8
JustAnika <darideh025@student.polsl.pl>
2022-06-07 23:13:56 +0200

    Fixed a bug where in edition one could set the date as the exact same as another visits for given doctor

--- PAzIG-wersja1/PAzIG/PAzIG/PAzIG/EditVisit.cs
+++ PAzIG-wersja1/PAzIG/PAzIG/PAzIG/EditVisit.cs
@@ -71,103 +71,131 @@ namespace PAzIG
         {
             string connection = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=Przychodnia;Integrated Security=True;Encrypt=False"; 
             SqlConnection con = new SqlConnection(connection);
-            if (laboratoryTextBox.Text != "")
+            string sqlQuery = "SELECT CONVERT(DATETIME,Data_wizyty,120) FROM Wizyta WHERE Id_pracownika LIKE '" + doctorTextBox.Text+"' AND Id_wizyty !=" + infoVisitLb.Text;
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sqlQuery, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            bool exists = false;
+            while (reader.Read())
             {
-                if (medicineTB.Text != "")
+                string[] data = reader[0].ToString().Split('.', ' ', ':');
+                int dzien = dateTimePicker1.Value.Day;
+                int miesiac = dateTimePicker1.Value.Month;
+                int rok = dateTimePicker1.Value.Year;
+                int godzina = dateTimePicker1.Value.Hour;
+                int minuta = dateTimePicker1.Value.Minute;
+                if (int.Parse(data[0]) == dzien && int.Parse(data[1]) == miesiac && int.Parse(data[2]) == rok && int.Parse(data[3]) == godzina && int.Parse(data[4]) == minuta)
                 {
-                    if (infoTextBox.Text != "")
+                    exists = true;
+                }
+
+            }
+            con.Close();
+            if (!exists)
+            {
+                if (laboratoryTextBox.Text != "")
+                {
+                    if (medicineTB.Text != "")
                     {
-                        //pracownia i leki i opis jest
-                        string data = "UPDATE Wizyta SET Id_pracowni="+ laboratoryTextBox.Text+", Lek='"+medicineTB.Text+"', Opis='"+infoTextBox.Text+"',Data_wizyty = CONVERT(DATETIME,'"+dateTimePicker1.Text+"',120) WHERE Id_wizyty="+infoVisitLb.Text;
-                        con.Open();
-                        SqlCommand newvisit = new SqlCommand(data, con);
-                        newvisit.ExecuteNonQuery();
-                        con.Close();
-                        MessageBox.Show("Edit succesful!");
+                        if (infoTextBox.Text != "")
+                        {
+                            //pracownia i leki i opis jest
+                            string data = "UPDATE Wizyta SET Id_pracowni=" + laboratoryTextBox.Text + ", Lek='" + medicineTB.Text + "', Opis='" + infoTextBox.Text + "',Data_wizyty = CONVERT(DATETIME,'" + dateTimePicker1.Text + "',120) WHERE Id_wizyty=" + infoVisitLb.Text;
+                            con.Open();
+                            SqlCommand newvisit = new SqlCommand(data, con);
+                            newvisit.ExecuteNonQuery();
+                            con.Close();
+                            MessageBox.Show("Edit succesful!");
+                        }
+                        else
+                        {
+                            //pracownia i leki ale bez opisu
+                            string data = "UPDATE Wizyta SET Id_pracowni=" + laboratoryTextBox.Text + ", Lek='" + medicineTB.Text + "',Data_wizyty = CONVERT(DATETIME,'" + dateTimePicker1.Text + "',120) WHERE Id_wizyty=" + infoVisitLb.Text;
+                            con.Open();
+                            SqlCommand newvisit = new SqlCommand(data, con);
+                            newvisit.ExecuteNonQuery();
+                            con.Close();
+                            MessageBox.Show("Edit succesful!");
+                        }
                     }
                     else
                     {
-                        //pracownia i leki ale bez opisu
-                        string data = "UPDATE Wizyta SET Id_pracowni=" + laboratoryTextBox.Text + ", Lek='" + medicineTB.Text+ "',Data_wizyty = CONVERT(DATETIME,'" + dateTimePicker1.Text + "',120) WHERE Id_wizyty=" + infoVisitLb.Text ;
-                        con.Open();
-                        SqlCommand newvisit = new SqlCommand(data, con);
-                        newvisit.ExecuteNonQuery();
-                        con.Close();
-                        MessageBox.Show("Edit succesful!");
+                        if (infoTextBox.Text != "")
+                        {
+                            //pracownia bez leków z opsiem
+                            string data = "UPDATE Wizyta SET Id_pracowni=" + laboratoryTextBox.Text + ", Opis='" + infoTextBox.Text + "',Data_wizyty = CONVERT(DATETIME,'" + dateTimePicker1.Text + "',120) WHERE Id_wizyty=" + infoVisitLb.Text;
+                            con.Open();
+                            SqlCommand newvisit = new SqlCommand(data, con);
+                            newvisit.ExecuteNonQuery();
+                            con.Close();
+                            MessageBox.Show("Edit succesful!");
+                        }
+                        else
+                        {
+                            //pracowia bez leków bez opisu
+                            string data = "UPDATE Wizyta SET Id_pracowni=" + laboratoryTextBox.Text + ",Data_wizyty = CONVERT(DATETIME,'" + dateTimePicker1.Text + "',120)" + " WHERE Id_wizyty=" + infoVisitLb.Text;
+                            con.Open();
+                            SqlCommand newvisit = new SqlCommand(data, con);
+                            newvisit.ExecuteNonQuery();
+                            con.Close();
+                            MessageBox.Show("Edit succesful!");
+                        }
                     }
                 }
                 else
                 {
-                    if (infoTextBox.Text != "")
+                    if (medicineTB.Text != "")
                     {
-                        //pracownia bez leków z opsiem
-                        string data = "UPDATE Wizyta SET Id_pracowni=" + laboratoryTextBox.Text + ", Opis='" + infoTextBox.Text + "',Data_wizyty = CONVERT(DATETIME,'" + dateTimePicker1.Text + "',120) WHERE Id_wizyty=" + infoVisitLb.Text ;
-                        con.Open();
-                        SqlCommand newvisit = new SqlCommand(data, con);
-                        newvisit.ExecuteNonQuery();
-                        con.Close();
-                        MessageBox.Show("Edit succesful!");
+                        if (infoTextBox.Text != "")
+                        {
+                            //bez pracowni, z lekami  i z opisem
+                            string data = "UPDATE Wizyta SET Lek='" + medicineTB.Text + "', Opis='" + infoTextBox.Text + "',Data_wizyty = CONVERT(DATETIME,'" + dateTimePicker1.Text + "',120) WHERE Id_wizyty=" + infoVisitLb.Text;
+                            con.Open();
+                            SqlCommand newvisit = new SqlCommand(data, con);
+                            newvisit.ExecuteNonQuery();
+                            con.Close();
+                            MessageBox.Show("Edit succesful!");
+                        }
+                        else
+                        {
+                            //bez pracowni z lekami bez opisu
+                            string data = "UPDATE Wizyta SET Lek='" + medicineTB.Text + "',Data_wizyty = CONVERT(DATETIME,'" + dateTimePicker1.Text + "',120) WHERE Id_wizyty=" + infoVisitLb.Text;
+                            con.Open();
+                            SqlCommand newvisit = new SqlCommand(data, con);
+                            newvisit.ExecuteNonQuery();
+                            con.Close();
+                            MessageBox.Show("Edit succesful!");
+                        }
                     }
                     else
                     {
-                        //pracowia bez leków bez opisu
-                        string data = "UPDATE Wizyta SET Id_pracowni=" + laboratoryTextBox.Text + ",Data_wizyty = CONVERT(DATETIME,'" + dateTimePicker1.Text + "',120)"+" WHERE Id_wizyty=" + infoVisitLb.Text ;
-                        con.Open();
-                        SqlCommand newvisit = new SqlCommand(data, con);
-                        newvisit.ExecuteNonQuery();
-                        con.Close();
-                        MessageBox.Show("Edit succesful!");
+                        if (infoTextBox.Text != "")
+                        {
+                            //bez leków bez pracowni , z opisem
+                            string data = "UPDATE Wizyta SET Opis='" + infoTextBox.Text + "',Data_wizyty = CONVERT(DATETIME,'" + dateTimePicker1.Text + "',120) WHERE Id_wizyty=" + infoVisitLb.Text;
+                            con.Open();
+                            SqlCommand newvisit = new SqlCommand(data, con);
+                            newvisit.ExecuteNonQuery();
+                            con.Close();
+                            MessageBox.Show("Edit succesful!");
+                        }
+                        else
+                        {
+                            string data = "UPDATE Wizyta SET Data_wizyty=CONVERT(DATETIME,'" + dateTimePicker1.Text + "',120) WHERE Id_wizyty=" + infoVisitLb.Text;
+                            con.Open();
+                            SqlCommand newvisit = new SqlCommand(data, con);
+                            newvisit.ExecuteNonQuery();
+                            con.Close();
+                            MessageBox.Show("Edit succesful!");
+                        }
                     }
                 }
             }
             else
             {
-                if (medicineTB.Text != "")
-                {
-                    if (infoTextBox.Text != "")
-                    {
-                        //bez pracowni, z lekami  i z opisem
-                        string data = "UPDATE Wizyta SET Lek='" + medicineTB.Text + "', Opis='" + infoTextBox.Text + "',Data_wizyty = CONVERT(DATETIME,'" + dateTimePicker1.Text + "',120) WHERE Id_wizyty=" + infoVisitLb.Text ;
-                        con.Open();
-                        SqlCommand newvisit = new SqlCommand(data, con);
-                        newvisit.ExecuteNonQuery();
-                        con.Close();
-                        MessageBox.Show("Edit succesful!");
-                    }
-                    else
-                    {
-                        //bez pracowni z lekami bez opisu
-                        string data = "UPDATE Wizyta SET Lek='" + medicineTB.Text + "',Data_wizyty = CONVERT(DATETIME,'" + dateTimePicker1.Text + "',120) WHERE Id_wizyty=" + infoVisitLb.Text;
-                        con.Open();
-                        SqlCommand newvisit = new SqlCommand(data, con);
-                        newvisit.ExecuteNonQuery();
-                        con.Close();
-                        MessageBox.Show("Edit succesful!");
-                    }
-                }
-                else
-                {
-                    if (infoTextBox.Text != "")
-                    {
-                        //bez leków bez pracowni , z opisem
-                        string data = "UPDATE Wizyta SET Opis='" + infoTextBox.Text + "',Data_wizyty = CONVERT(DATETIME,'" + dateTimePicker1.Text + "',120) WHERE Id_wizyty=" + infoVisitLb.Text;
-                        con.Open();
-                        SqlCommand newvisit = new SqlCommand(data, con);
-                        newvisit.ExecuteNonQuery();
-                        con.Close();
-                        MessageBox.Show("Edit succesful!");
-                    }
-                    else
-                    {
-                        string data = "UPDATE Wizyta SET Data_wizyty=CONVERT(DATETIME,'"+dateTimePicker1.Text+"',120) WHERE Id_wizyty=" + infoVisitLb.Text;
-                        con.Open();
-                        SqlCommand newvisit = new SqlCommand(data, con);
-                        newvisit.ExecuteNonQuery();
-                        con.Close();
-                        MessageBox.Show("Edit succesful!");
-                    }
-                }
+                MessageBox.Show("There already exists a visit for this doctor at given date!");
             }
+           
         }
     }
 }
